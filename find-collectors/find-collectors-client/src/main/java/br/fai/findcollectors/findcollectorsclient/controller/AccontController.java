b14fb8880253cbb15cc0c9eace2ffb6b6ed24804
anilson <pcassioferreira@gmail.com>
2022-09-08 20:43:08 -0300

    feat: :art: adicionado tela login

---
+++ find-collectors/find-collectors-client/src/main/java/br/fai/findcollectors/findcollectorsclient/controller/AccontController.java
@@ -0,0 +1,17 @@
+package br.fai.findcollectors.findcollectorsclient.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccontController {
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "account/access";
+    }
+
+}
