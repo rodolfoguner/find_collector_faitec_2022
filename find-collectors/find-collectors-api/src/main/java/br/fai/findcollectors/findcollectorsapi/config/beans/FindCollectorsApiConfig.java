e35e2623db8b156b22296e63b8574a593be66847
enthony <enthonyedu@gmail.com>
2022-10-05 20:48:35 -0300

    refactor: removido o bean de recycler

--- find-collectors/find-collectors-api/src/main/java/br/fai/findcollectors/findcollectorsapi/config/beans/FindCollectorsApiConfig.java
+++ find-collectors/find-collectors-api/src/main/java/br/fai/findcollectors/findcollectorsapi/config/beans/FindCollectorsApiConfig.java
@@ -2,9 +2,14 @@ package br.fai.findcollectors.findcollectorsapi.config.beans;
 
 import br.fai.findcollectors.entities.Collect;
 import br.fai.findcollectors.entities.Person;
-import br.fai.findcollectors.entities.Recycler;
-import br.fai.findcollectors.findcollectorsdatabase.dao.*;
-import br.fai.findcollectors.findcollectorsdatabase.dao.impl.*;
+import br.fai.findcollectors.findcollectorsdatabase.dao.CityDao;
+import br.fai.findcollectors.findcollectorsdatabase.dao.CollectDao;
+import br.fai.findcollectors.findcollectorsdatabase.dao.PersonDao;
+import br.fai.findcollectors.findcollectorsdatabase.dao.StateDao;
+import br.fai.findcollectors.findcollectorsdatabase.dao.impl.CityDaoImpl;
+import br.fai.findcollectors.findcollectorsdatabase.dao.impl.CollectDaoImpl;
+import br.fai.findcollectors.findcollectorsdatabase.dao.impl.PersonDaoImpl;
+import br.fai.findcollectors.findcollectorsdatabase.dao.impl.StateDaoImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,11 +24,6 @@ public class FindCollectorsApiConfig {
     @Bean
     public PersonDao<Person> getPersonDao() {
         return new PersonDaoImpl();
-    }
-
-    @Bean
-    RecyclerDao<Recycler> getRecyclerDao() {
-        return new RecyclerDaoImpl();
     }
 
     @Bean
