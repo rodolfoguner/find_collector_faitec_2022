5adbe6140d6887d492d31b638a7bde13f5d296a5
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-10-05 13:14:22 -0300

    feat: adicionado regras do authenticate

--- find-collectors/find-collectors-client/src/main/java/br/fai/findcollectors/findcollectorsclient/config/security/providers/FindCollectorsAuthenticationProvider.java
+++ find-collectors/find-collectors-client/src/main/java/br/fai/findcollectors/findcollectorsclient/config/security/providers/FindCollectorsAuthenticationProvider.java
@@ -1,18 +1,42 @@
 package br.fai.findcollectors.findcollectorsclient.config.security.providers;
 
 
+import br.fai.findcollectors.entities.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class FindCollectorsAuthenticationProvider implements AuthenticationProvider {
 
+//    @Autowired
+//    PersonService personService;
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+//        Person person = personService.validateLogin(username, password);
+
+//        if (person == null) {
+//            return null;
+//        }
+
+        grantedAuthorities.add(new SimpleGrantedAuthority(""));
+
+        return new UsernamePasswordAuthenticationToken(new Person(), password, grantedAuthorities);
     }
 
     @Override
