7727e3fa99da1d659db149b79855649dcb573e9d
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-10-05 13:18:31 -0300

    refactor: alterado o controller accont para account

--- find-collectors/find-collectors-client/src/main/java/br/fai/findcollectors/findcollectorsclient/controller/AccountController.java
+++ find-collectors/find-collectors-client/src/main/java/br/fai/findcollectors/findcollectorsclient/controller/AccountController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/account")
-public class AccontController {
+public class AccountController {
 
     @GetMapping("/sign-in")
     public String getLoginPage() {
