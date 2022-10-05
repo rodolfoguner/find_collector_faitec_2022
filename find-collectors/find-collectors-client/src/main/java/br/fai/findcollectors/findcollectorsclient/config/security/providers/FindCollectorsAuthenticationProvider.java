a1d33f4680991c790543847250a27cf7f26b04bf
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-10-05 12:53:50 -0300

    feat: adicionado provider de autenticacao

--- find-collectors/find-collectors-client/src/main/java/br/fai/findcollectors/findcollectorsclient/config/security/providers/FindCollectorsAuthenticationProvider.java
+++ find-collectors/find-collectors-client/src/main/java/br/fai/findcollectors/findcollectorsclient/config/security/providers/FindCollectorsAuthenticationProvider.java
@@ -1,4 +1,22 @@
 package br.fai.findcollectors.findcollectorsclient.config.security.providers;
 
-public class FindCollectorsAuthenticationProvider {
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FindCollectorsAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
 }
