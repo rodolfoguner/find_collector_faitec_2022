3ba6843b8e7383f16a85266718beef3bed53459e
enthony <enthonyedu@gmail.com>
2022-10-05 20:30:06 -0300

    refactor: removido todas as referencias da classe Collector

--- find-collectors/find-collectors-api/src/main/java/br/fai/findcollectors/findcollectorsapi/service/CollectorRestService.java
+++
@@ -1,4 +0,0 @@
-package br.fai.findcollectors.findcollectorsapi.service;
-
-public interface CollectorRestService<T> extends BaseRestService<T> {
-}
--- find-collectors/find-collectors-database/src/main/java/br/fai/findcollectors/findcollectorsdatabase/dao/CollectorDao.java
+++
@@ -1,4 +0,0 @@
-package br.fai.findcollectors.findcollectorsdatabase.dao;
-
-public interface CollectorDao<T> extends BaseDao<T> {
-}
