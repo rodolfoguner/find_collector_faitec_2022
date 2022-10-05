5aa24cddb8a3217d223a704b273e21d411c4d47f
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-10-05 00:48:13 -0300

    feat: adicionado interface BaseServicec

---
+++ find-collectors/find-collectors-client/src/main/java/br/fai/findcollectors/findcollectorsclient/service/BaseService.java
@@ -0,0 +1,17 @@
+package br.fai.findcollectors.findcollectorsclient.service;
+
+import java.util.List;
+
+public interface BaseService<T> {
+
+    int create(T entity);
+
+    List<T> find();
+
+    T findById(int id);
+
+    boolean update(int id, T entity);
+
+    boolean deleteById(int id);
+    
+}
