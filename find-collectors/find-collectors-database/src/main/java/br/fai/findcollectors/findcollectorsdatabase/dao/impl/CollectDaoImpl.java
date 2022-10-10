dae8244a4746111e182fbf0e16653f103bfbbbfe
Enthony <enthonyedu@gmail.com>
2022-10-10 18:40:12 -0300

    refactor: jogado os metodos de find para loadvalues

--- find-collectors/find-collectors-database/src/main/java/br/fai/findcollectors/findcollectorsdatabase/dao/impl/CollectDaoImpl.java
+++ find-collectors/find-collectors-database/src/main/java/br/fai/findcollectors/findcollectorsdatabase/dao/impl/CollectDaoImpl.java
@@ -1,23 +1,34 @@
 package br.fai.findcollectors.findcollectorsdatabase.dao.impl;
 
+import br.fai.findcollectors.entities.City;
 import br.fai.findcollectors.entities.Collect;
+import br.fai.findcollectors.enums.GarbageType;
 import br.fai.findcollectors.findcollectorsdatabase.connection.ConnectionFactory;
+import br.fai.findcollectors.findcollectorsdatabase.dao.CityDao;
 import br.fai.findcollectors.findcollectorsdatabase.dao.CollectDao;
+import br.fai.findcollectors.findcollectorsdatabase.dao.PersonDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class CollectDaoImpl implements CollectDao<Collect> {
+
+    @Autowired
+    CityDao cityDao;
+
+    @Autowired
+    PersonDao personDao;
+
     @Override
     public List<Collect> find() {
         List<Collect> collects = new ArrayList<Collect>();
+
         final String sql = "SELECT * FROM coleta";
+
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
@@ -28,19 +39,7 @@ public class CollectDaoImpl implements CollectDao<Collect> {
             resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                Collect collect = new Collect();
-
-                collect.setId(resultSet.getInt("id"));
-                collect.setDateAndTime(resultSet.getTimestamp("data_e_hora"));
-                collect.setAccept(resultSet.getBoolean("aceito"));
-                collect.setCollected(resultSet.getBoolean("coletado"));
-                collect.setRecurrent(resultSet.getBoolean("recorrente"));
-                collect.setCep(resultSet.getString("cep"));
-                collect.setAddress(resultSet.getString("endereco"));
-                collect.setDistrict(resultSet.getString("bairro"));
-                collect.setNumber(resultSet.getString("numero"));
-                collect.setCreatedAt(resultSet.getTimestamp("criado_em"));
-                collect.setLastModified(resultSet.getTimestamp("alterado_em"));
+                Collect collect = loadValues(resultSet);
 
                 collects.add(collect);
             }
@@ -74,6 +73,38 @@ public class CollectDaoImpl implements CollectDao<Collect> {
 
     @Override
     public Collect loadValues(ResultSet resultSet) throws SQLException {
-        return null;
+
+        Collect collect = new Collect();
+        City city = cityDao.findById(resultSet.getInt("municipio_id"));
+        Array garbageTypes = resultSet.getArray("tipo_lixo");
+        List<GarbageType> garbageTypeList = new ArrayList<>();
+
+        if (garbageTypes != null) {
+
+            String garbageTypesArray[] = (String[]) garbageTypes.getArray();
+
+            for (String garbageType : garbageTypesArray) {
+
+                garbageTypeList.add(Enum.valueOf(GarbageType.class, garbageType));
+            }
+        }
+
+        collect.setId(resultSet.getInt("id"));
+        collect.setDateAndTime(resultSet.getTimestamp("data_e_hora"));
+        collect.setGarbageType(garbageTypeList);
+        collect.setAccept(resultSet.getBoolean("aceito"));
+        collect.setCollected(resultSet.getBoolean("coletado"));
+        collect.setRecurrent(resultSet.getBoolean("recorrente"));
+        collect.setCep(resultSet.getString("cep"));
+        collect.setAddress(resultSet.getString("endereco"));
+        collect.setDistrict(resultSet.getString("bairro"));
+        collect.setNumber(resultSet.getString("numero"));
+        collect.setCity(city);
+        collect.setCityId(resultSet.getInt("municipio_id"));
+        collect.setCollector();
+        collect.setCreatedAt(resultSet.getTimestamp("criado_em"));
+        collect.setLastModified(resultSet.getTimestamp("alterado_em"));
+
+        return collect;
     }
 }
