89ed8fec44a831a6b56dba01918392acdb556ce0
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-10-11 20:09:12 -0300

    feat: adicionado edit-profile page

--- find-collectors/find-collectors-client/src/main/java/br/fai/findcollectors/findcollectorsclient/controller/AccountController.java
+++ find-collectors/find-collectors-client/src/main/java/br/fai/findcollectors/findcollectorsclient/controller/AccountController.java
@@ -5,9 +5,12 @@ import br.fai.findcollectors.entities.Person;
 import br.fai.findcollectors.findcollectorsclient.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/account")
@@ -26,7 +29,21 @@ public class AccountController {
         return "account/sign-up-page";
     }
 
-    @PostMapping("sign-up")
+    @GetMapping("/edit-profile")
+    public String getEditProfilePage(final Model model, final HttpSession session) {
+
+        Person person = (Person) session.getAttribute("currentUser");
+
+        if (person == null) {
+            return "redirect:/common/not-found";
+        }
+
+        model.addAttribute("currentUser", person);
+
+        return "account/edit-profile";
+    }
+
+    @PostMapping("/sign-up")
     public String createLogin(Person person) {
 
         int created = personService.create(person);
--- find-collectors/find-collectors-client/src/main/java/br/fai/findcollectors/findcollectorsclient/controller/HomeController.java
+++ find-collectors/find-collectors-client/src/main/java/br/fai/findcollectors/findcollectorsclient/controller/HomeController.java
@@ -1,13 +1,23 @@
 package br.fai.findcollectors.findcollectorsclient.controller;
 
+import br.fai.findcollectors.entities.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String getHomePage() {
+    public String getHomePage(HttpSession session) {
+
+        Person person = (Person) session.getAttribute("currentUser");
+
+        if (person != null && person.getCity() == null) {
+            return "redirect:/account/edit-profile";
+        }
+
         return "index";
     }
 }
