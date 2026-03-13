4a73e21b7d80dffba9b74104df36324c95861362
Rodolfo Mamud <rodolfo.guner@outlook.com>
2026-03-13 00:23:05 -0300

    docs: improvement on swagger docs

--- find-collectors/api/src/main/java/br/fai/findcollectors/config/SwaggerConfig.java
+++ find-collectors/api/src/main/java/br/fai/findcollectors/config/SwaggerConfig.java
@@ -1,11 +1,22 @@
 package br.fai.findcollectors.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Find Collectors API")
+                        .version("1.0")
+                        .description("API para gerenciamento de coleta reciclável"));
+    }
 
     @Bean
     public GroupedOpenApi publicApi(){
@@ -17,5 +28,4 @@ public class SwaggerConfig {
                 .build();
 
     }
-
 }
